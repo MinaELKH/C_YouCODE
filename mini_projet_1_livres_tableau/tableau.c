3f17bc8a6596fc1e610fb4d85b6fb019bb214028
MinaELKH <mina.elkhotfi@gmail.com>
2024-09-27 21:28:31 +0100

    mini Projet v2

--- mini_projet_1_livres_tableau/tableau.c
+++ mini_projet_1_livres_tableau/tableau.c
@@ -20,25 +20,124 @@ void Get();
 int Find();       
 void SetQte();      
 void Delete();    
-void GetQte();      
+void GetQte();
+void menu();
 
 int main() {
-    int choix;
+    menu(); 
+    return 0;
+}
+void  Create() {
+    int i = compteur++;  
+    printf("******** Ajouter un livre **********\n"); 
+    
+    printf("Titre du livre : ");fgets(title[i], 50, stdin);
+    title[i][strcspn(title[i], "\n")] = '\0';
+    
+    printf("Auteur du livre : "); fgets(autor[i], 50, stdin);
+    autor[i][strcspn(autor[i], "\n")] = '\0';
+    
+    printf("Prix du livre : ") ; scanf("%f", &price[i]);
+    printf("Quantite disponible : "); scanf("%d", &qte[i]);
 
-    while (1) {  
+    printf("\nLivre ajoute avec succes !\n");
+}
+void Get(){
+    printf("*************************************** Liste de livres *****************************\n"); 
+    printf("+------------------------------------------+---------------------+--------+---------+\n");
+    printf("| Titre                                    | Auteur              | Prix   | Quantite|\n");
+    printf("+------------------------------------------+---------------------+--------+---------+\n");
+
+    for(int i = 1 ; i < compteur ;i++)
+    {
+        printf("| %-40s | %-19s | %-6.2f | %-7d|\n", title[i] , autor[i] , price[i] , qte[i]);
+    }
+    printf("+------------------------------------------+---------------------+--------+---------+\n");
+}         
+int Find(){
+     printf("******** Chercher un livre **********\n"); 
+     char name[sizeTitle] ; 
+     bool find = false ;
+     int i; 
+     printf("Titre du livre recherche: ");
+     fgets(name, 50, stdin);
+     name[strcspn(name, "\n")] = 0;
+    printf("+------------------------------------------+---------------------+--------+---------+\n");
+    printf("| Titre                                    | Auteur              | Prix   | Quantite|\n");
+    printf("+------------------------------------------+---------------------+--------+---------+\n");
+
+      for(i = 1 ; i < compteur ;i++)
+      {
+         if (strcmp((name) ,title[i] )==0) {
+         find = true ; 
+          printf("| %-40s | %-19s | %-6.2f | %-7d|\n", title[i] , autor[i] , price[i] , qte[i]);
+         break ; 
+       } 
+     }
+        printf("+------------------------------------------+---------------------+--------+---------+\n");
+    if (find = true) {
+         return i ; 
+    } else {
+         printf("Nom de livre invalide. Veuillez reessayer.\n");
+        return 0 ;
+    }
+}         
+void SetQte(){
+    int i = Find(); 
+    if (i != 0 ){
+         printf("\n Pourrez-vous entrer la nouvelle quantite  : ");
+         scanf("%d",&qte[i]); 
+    } else {printf("Nom de livre invalide. Veuillez reessayer.\n ");}
+}        
+void Delete(){
+     int i = Find();
+     bool deleted = false;
+     if (i != 0 ){
+         for ( ; i < compteur-1; i++)
+                {
+                    strcpy(title[i], title[i + 1]);
+                    strcpy(autor[i], autor[i + 1]);
+                    price[i] = price[i + 1];
+                    qte[i] = qte[i+1];
+                    
+                }
+        deleted = true;
+        printf("\nLivre a ete supprime avec succes !\n");
+        compteur--;
+     }
+  else {printf("Erreur ");}
+   
+}   
+
+void GetQte(){
+    int sum=0; 
+    for (int i = 1 ; i <= compteur; i++)
+    {
+        sum = sum +qte[i]; 
+     
+     }
+
+        printf("\n la somme de livre est %d : " , sum);
+}    
+
+
+void menu(){
+    int choice ,  reponse;
+    do
+    {  
         printf("\n******** Menu de Gestion de Stock ********\n");
         printf("1. Ajouter un livre au stock.\n");
         printf("2. Afficher tous les livres disponibles.\n");
         printf("3. Rechercher un livre par son titre.\n");
-        printf("4. Mettre à jour la quantité d'un livre.\n");
+        printf("4. Mettre a jour la quantite d'un livre.\n");
         printf("5. Supprimer un livre du stock.\n");
         printf("6. Afficher le nombre total de livres en stock.\n");
         printf("7. Quitter.\n");
         printf("Votre choix : ");
-        scanf("%d", &choix);
+        scanf("%d", &choice);
         getchar();
 
-        switch (choix) {
+        switch (choice) {
             case 1:
                 Create();
                 break;
@@ -61,76 +160,14 @@ int main() {
                 printf("Au revoir !\n");
                 exit(0);  
             default:
-                printf("Choix invalide. Veuillez réessayer.\n");
+                printf("Choix invalide. Veuillez reessayer.\n");
         }
-    }
-
-    return 0;
+        printf("\nTapez '1' pour Revenir au menu ou '0' pour quitter : ");
+        scanf("%d", &reponse);
+        getchar(); 
+        if (reponse == '0') {
+            printf("Au revoir !\n");
+            exit(0);
+        }
+    } while (reponse != '1'); 
 }
-void  Create() {
-    int i = compteur++;  
-    printf("******** Ajouter un livre **********\n"); 
-    
-    printf("Titre du livre : ");fgets(title[i], 50, stdin);
-    title[i][strcspn(title[i], "\n")] = '\0';
-    
-    printf("Auteur du livre : "); fgets(autor[i], 50, stdin);
-    autor[i][strcspn(autor[i], "\n")] = '\0';
-    
-    printf("Prix du livre : ") ; scanf("%f", &price[i]);
-    printf("Quantite disponible : "); scanf("%d", &qte[i]);
-
-    printf("\nLivre ajoute avec succes !\n");
-}
-void Get(){
-     printf("******** Liste de livres **********\n"); 
-      printf("\nTitre\t Auteur\t Prix\t Quantite\f \n") ;
-    for(int i = 1 ; i < compteur ;i++)
-    {
-         printf(" %s\t %s\t %.2f\t %d\t \n  " ,title[i] , autor[i] , price[i] , qte[i] );
-    }
-}         
-int Find(){
-     printf("******** Chercher un livre **********\n"); 
-     char name[sizeTitle] ; 
-     bool find = false ;
-     int i; 
-     printf("Titre du livre recherche: ");
-     fgets(name, 50, stdin);
-     name[strcspn(name, "\n")] = 0;
-
-      for(i = 1 ; i < compteur ;i++)
-      {
-         if (strcmp((name) ,title[i] )==0) {
-         find = true ; 
-         printf("Titre\t Auteur\t Prix\t Quantite\f \n %s\t %s\t %.2f\t %d\t  " ,title[i] , autor[i] , price[i] , qte[i] );
-         break ; 
-    }  }
-    if (find = true) {
-         return i ; 
-    } else {
-         printf("\nle livre n est pas trouve");
-        return 0 ;
-    }
-}         
-void SetQte(){
-    int i = Find(); 
-    if (i != 0 ){
-         printf("\nle livre est trouve , vous pourrez entrer la nouvelle quantite");
-         scanf("%d",&qte[i]); 
-    } else {printf("Veuillez entrer le livre d abord ");}
-}        
-void Delete(){
-     int i = Find();
-     if (i != 0 ){
-           for (i; i < compteur; i++)
-     {
-         title[i] = title[i + 1];
-         autor[i] = autor[i+1];
-         price[i] = price[i + 1];
-         qte[i] = qte[]; 
-     }
-    } else {printf("Veuillez entrer le livre d abord ");}
-   
-}      
-void GetQte(){}    
